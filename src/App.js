cad3d51a94195aeedc65eca5e7fceee1e8d5ddaa
Le Dinh Kien <s3515300@rmit.edu.vn>
2016-12-15 23:30:44 +0700

    Change page title

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
                 });
 
             } else {
-                console.log('false');
+                // console.log('false');
                 this.setState({loggedIn: false});
             }
         });
