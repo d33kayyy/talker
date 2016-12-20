99fb3452f29c28d8026b6a73a1202ce13e0b4c58
Le Dinh Kien <s3515300@rmit.edu.vn>
2016-12-20 22:39:14 +0700

    switch database

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDucSFmf7ISfPOaE-WZXAv4biMULPo3zxY",
-    authDomain: "talker-37ce2.firebaseapp.com",
-    databaseURL: "https://talker-37ce2.firebaseio.com",
-    storageBucket: "talker-37ce2.appspot.com",
-    messagingSenderId: "635070252811"
+    // apiKey: "AIzaSyDucSFmf7ISfPOaE-WZXAv4biMULPo3zxY",
+    // authDomain: "talker-37ce2.firebaseapp.com",
+    // databaseURL: "https://talker-37ce2.firebaseio.com",
+    // storageBucket: "talker-37ce2.appspot.com",
+    // messagingSenderId: "635070252811"
 
-    // apiKey: "AIzaSyCdZ0EKNzN5v8VEJly2KykdOzZ0eQjCwwQ",
-    // authDomain: "talkerproject-934bc.firebaseapp.com",
-    // databaseURL: "https://talkerproject-934bc.firebaseio.com",
-    // storageBucket: "talkerproject-934bc.appspot.com",
-    // messagingSenderId: "366757086342"
+    apiKey: "AIzaSyCdZ0EKNzN5v8VEJly2KykdOzZ0eQjCwwQ",
+    authDomain: "talkerproject-934bc.firebaseapp.com",
+    databaseURL: "https://talkerproject-934bc.firebaseio.com",
+    storageBucket: "talkerproject-934bc.appspot.com",
+    messagingSenderId: "366757086342"
 };
 firebase.initializeApp(config);
 
